7f9f55480ae5be8e3b793aa0227e86a4fd18b414
marouane-m7b <marouane.ma7boub@gmail.com>
2026-05-11 14:12:17 +0100

    Initialize Spring Boot project

---
+++ src/main/java/net/mahboub/mahboubmarouaneexamjee/MahboubMarouaneExamJeeApplication.java
@@ -0,0 +1,13 @@
+package net.mahboub.mahboubmarouaneexamjee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahboubMarouaneExamJeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahboubMarouaneExamJeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/mahboub/mahboubmarouaneexamjee/MahboubMarouaneExamJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mahboub.mahboubmarouaneexamjee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahboubMarouaneExamJeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
